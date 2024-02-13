8acd9de14ec52694aa574c87b872502442a53baa
anjalika27 <112751110+anjalika27@users.noreply.github.com>
2024-02-14 00:34:06 +0530

    Update settings.py

--- moveasy/moveasy/settings.py
+++ moveasy/moveasy/settings.py
@@ -133,5 +133,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "anjalika.agarwal@gmail.com"
-EMAIL_HOST_PASSWORD = "ywek hcvd jbck vong"
+EMAIL_HOST_USER = "your@gmail.com"
+EMAIL_HOST_PASSWORD = "your_password"
